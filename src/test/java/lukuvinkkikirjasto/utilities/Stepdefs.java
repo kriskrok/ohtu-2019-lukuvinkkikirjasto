b80f0b6fb30ee0dffb176b035c83ae82a32836c7
Saara Satokangas <saarasat@users.noreply.guthub.com>
2019-11-23 12:45:45 +0200

    Added first Cucumber-tests and Selenium

--- src/test/java/lukuvinkkikirjasto/utilities/Stepdefs.java
+++ src/test/java/lukuvinkkikirjasto/utilities/Stepdefs.java
@@ -1,4 +1,6 @@
 package lukuvinkkikirjasto.utilities;
+import lukuvinkkikirjasto.domain.*;
+import lukuvinkkikirjasto.utilities.*;
 
 import io.cucumber.java.Before;
 import io.cucumber.java.en.Given;
@@ -7,28 +9,54 @@ import io.cucumber.java.en.Then;
 import java.util.ArrayList;
 import java.util.List;
 import static org.junit.Assert.*;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 
-import lukuvinkkikirjasto.domain.*;
-import lukuvinkkikirjasto.utilities.*;
 
 public class Stepdefs {
-    Application app;
+    WebDriver driver = new HtmlUnitDriver();
+    String baseUrl = "http://localhost:4567";
     
-    @Before
-    public void setup(){
-    }
-    
-    @Given("^command lisaa uusi kirja is selected$")
+    // Tests for adding a new book 
+    @Given("lisaa kirja lukuvinkkeihin is selected")
     public void commandAddNewBookSelected() throws Throwable {
+        driver.get(baseUrl);
+        WebElement element = driver.findElement(By.linkText("Lisää kirja lukuvinkkeihin"));
+        element.click();  
     }
 
-    @When("booktitle {string} and writer {string} are entered")
-    public void bookTitleAndWriterAreEntered(String bookTitle, String writer) {       
-    }
     
+    @When("a valid booktitle {string} and writer {string} are entered")
+    public void validBookTitleAndWriterAreEntered(String bookTitle, String writer) {
+        bookTitleAndWriterAreGiven(bookTitle, writer);
+    }
+
+    @When("an empty booktitle {string} and writer {string} are entered")
+    public void emplyBookTitleIsEntered(String bookTitle, String writer) {
+        bookTitleAndWriterAreGiven(bookTitle, writer);
+    }
+
     @Then("system will respond with {string}")
     public void systemWillRespondWith(String expectedOutput) {
+        pageHasContent(expectedOutput);
     }
- 
+
+    //helper-methods
+    private void pageHasContent(String content) {
+        assertTrue(driver.getPageSource().contains(content));
+    }
+
+    private void bookTitleAndWriterAreGiven(String bookTitle, String writer) {
+        pageHasContent("Syötä lukuvinkkikirjan tiedot:");
+        WebElement element = driver.findElement(By.name("kirjan_nimi"));
+        element.sendKeys(bookTitle);
+        element = driver.findElement(By.name("kirjoittaja"));
+        element.sendKeys(writer);
+        element = driver.findElement(By.name("lisaa"));
+        element.submit(); 
+    }
 
 }
--- src/test/java/resources/vinkkienLisaaminen.feature
+++ src/test/java/resources/vinkkienLisaaminen.feature
@@ -1,2 +1,12 @@
-Feature: tänne tulloo aikanaan kurkkumuotoisia käyttötapauksia
+Feature: User can add a valid book to reading tips
+
+  Scenario: user can add a valid book
+    Given lisaa kirja lukuvinkkeihin is selected
+    When a valid booktitle "Tira-kirja" and writer "Antti Laaksonen" are entered
+    Then system will respond with "Tira-kirja tallennettu!"
+
+  Scenario: user can not a book without name
+    Given lisaa kirja lukuvinkkeihin is selected
+    When an empty booktitle "" and writer "Pekkaliisa Peukaloinen" are entered
+    Then system will respond with "Täytäthän kummatkin tiedot!"
 
