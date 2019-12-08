afdc7b55ce34881f0a1449d67445e48464d840c2
Laura S <46318664+sinikala@users.noreply.github.com>
2019-12-09 00:13:36 +0200

    edits Lukuvinkki class

--- src/main/java/lukuvinkkikirjasto/domain/Lukuvinkki.java
+++ src/main/java/lukuvinkkikirjasto/domain/Lukuvinkki.java
@@ -3,11 +3,11 @@ package lukuvinkkikirjasto.domain;
 public class Lukuvinkki implements Comparable<Lukuvinkki> {
     public int id;
 
-    public int getId() {
+    /*public int getId() {
         return id;
     }
 
-
+*/
     @Override
     public int compareTo(Lukuvinkki another) {
         return another.id - this.id;
